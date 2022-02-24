a642191b9f49874d5656cd0eaa14d2d5604d6a58
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-02-24 13:08:37 +0300

    Add project files.

---
+++ WebAPIBank_0/DesignPatterns/StrategyPattern/Init/MyInit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAPIBank_0.Models.Context;
+using WebAPIBank_0.Models.Entities;
+
+namespace WebAPIBank_0.DesignPatterns.StrategyPattern.Init
+{
+    public class MyInit:CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            CardInformation ci = new CardInformation();
+            ci.CardUserName = "Yalcın Tan";
+            ci.CardNumber = "1111 1111 1111 1111";
+            ci.CardExpiryYear = 2024;
+            ci.CardExpiryMonth = 10;
+            ci.SecurityNumber = "222";
+            ci.Limit = 50000;
+            ci.Balance = 50000;
+            context.Cards.Add(ci);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ WebAPIBank_0/Models/Context/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAPIBank_0.DesignPatterns.StrategyPattern.Init;
+using WebAPIBank_0.Models.Entities;
+
+namespace WebAPIBank_0.Models.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+            Database.SetInitializer(new MyInit());
+        }
+
+        public DbSet<CardInformation> Cards { get; set; }
+    }
+}
